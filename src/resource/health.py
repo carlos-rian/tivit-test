66f06098ef759e7fe6bc3cd695ec411f69c058a1
carlos-rian <crian.rian@gmail.com>
2025-01-04 20:04:57 -0300

    feat: add logging for health check endpoint status

--- src/resource/health.py
+++ src/resource/health.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status
 from fastapi.responses import ORJSONResponse
 
 from src.common.basedb import DatabasePool
+from src.common.logger import Logger
 from src.schema.health import GetHealthOut
 
 router = APIRouter()
@@ -12,10 +13,12 @@ async def get_health():
 	"""
 	This endpoint is used to check the health of the service.
 	"""
+	Logger.info("Checking the health of the API")
 	if DatabasePool.pool.closed:
+		Logger.error("API is not healthy")
 		return ORJSONResponse(
 			status_code=status.HTTP_417_EXPECTATION_FAILED,
 			content=GetHealthOut(status="error", message="API is not healthy").model_dump(by_alias=True),
 		)
-
+	Logger.info("API is healthy")
 	return GetHealthOut(status="ok", message="API is healthy")
